22afd2023297f1c1b1b6cf7800acd74d1abd4dec
Alec Aivazis <alec@aivazis.com>
2018-12-29 19:13:55 -0800

    added failing tests

--- planning_test.go
+++ planning_test.go
@@ -1,1 +1,23 @@
 package gateway
+
+import "testing"
+
+func TestPlanQuery_singleRootField(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+func TestPlanQuery_multipleRootFields(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+func TestPlanQuery_mutationsInSeries(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+func TestPlanQuery_subGraphs(t *testing.T) {
+	t.Error("Not implemented")
+}
+
+func TestPlanQuery_siblingFields(t *testing.T) {
+	t.Error("Not implemented")
+}
