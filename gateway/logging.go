4c1b47a6746a0ed4716d6fd784e6b868c4b99eb0
Alec Aivazis <alec@aivazis.com>
2018-12-31 01:23:24 -0800

    removed unused declarations

--- gateway/logging.go
+++ gateway/logging.go
@@ -12,21 +12,6 @@ func (l *Logger) Debug(args ...interface{}) {
 	logrus.Debug(args...)
 }
 
-// Info should be used for tracking a noteworthy event
-func (l *Logger) Info(args ...interface{}) {
-	logrus.Info(args...)
-}
-
-// Warn should be used to log something that is worth some attention
-func (l *Logger) Warn(args ...interface{}) {
-	logrus.Warn(args...)
-}
-
-// Error should be used to log something that definitely wants attention
-func (l *Logger) Error(args ...interface{}) {
-	logrus.Error(args...)
-}
-
 var log *Logger
 
 func init() {
--- gateway/testing.go
+++ gateway/testing.go
@@ -3,16 +3,10 @@ package gateway
 import (
 	"github.com/vektah/gqlparser"
 	"github.com/vektah/gqlparser/ast"
-	"github.com/vektah/gqlparser/gqlerror"
-	"github.com/vektah/gqlparser/parser"
 )
 
 func loadSchema(typedef string) (*ast.Schema, error) {
 	return gqlparser.LoadSchema(&ast.Source{
 		Input: typedef,
 	})
-}
-
-func parseQuery(query string) (*ast.QueryDocument, *gqlerror.Error) {
-	return parser.ParseQuery(&ast.Source{Input: query})
 }
