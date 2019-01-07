987ab78f88ce4cbecfa4607dc5b9e0ce0fff3fca
Alec Aivazis <alec@aivazis.com>
2019-01-07 02:39:15 -0800

    Feat/requests (#21)
    * added tests
    
    * added query printer
    
    * cleanup and name changes
    
    * added tests for query variable printing
    
    * added test for printing directives
    
    * added tests for operation names
    
    * added test for printing boolean values
    
    * added variable definitions to operation
    
    * added tests for requests
    
    * cleanup

--- gateway/requests.go
+++ gateway/requests.go
@@ -1,6 +1,13 @@
 package gateway
 
-import "github.com/vektah/gqlparser/ast"
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/vektah/gqlparser/ast"
+)
 
 // Queryer is a interface for objects that can perform
 type Queryer interface {
@@ -9,7 +16,7 @@ type Queryer interface {
 
 // MockQueryer responds with pre-defined known values when executing a query
 type MockQueryer struct {
-	Value map[string]interface{}
+	Value JSONObject
 }
 
 // Query looks up the name of the query in the map of responses and returns the value
@@ -19,10 +26,58 @@ func (q *MockQueryer) Query(query *ast.QueryDocument) (map[string]interface{}, e
 
 // NetworkQueryer sends the query to a url and returns the response
 type NetworkQueryer struct {
-	URL string
+	URL    string
+	Client *http.Client
 }
 
 // Query sends the query to the designated url and returns the response.
 func (q *NetworkQueryer) Query(query *ast.QueryDocument) (map[string]interface{}, error) {
-	return map[string]interface{}{}, nil
+	// grab the operation
+	operation := query.Operations[0]
+
+	// turn the query into a string
+	queryStr, err := PrintQuery(operation)
+	if err != nil {
+		return nil, err
+	}
+
+	// the payload
+	payload, err := json.Marshal(JSONObject{
+		"operationName": operation.Name,
+		"query":         queryStr,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// fire the response to the queryer's url
+	resp, err := q.Client.Post(q.URL, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return nil, err
+	}
+	// read the full body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// parse the response as json
+	response := JSONObject{}
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	// pass the result along
+	return response, nil
+}
+
+// NewNetworkQueryer returns a NetworkQueryer pointed to the given url
+func NewNetworkQueryer(url string) *NetworkQueryer {
+	return &NetworkQueryer{
+		URL:    url,
+		Client: &http.Client{},
+	}
 }
