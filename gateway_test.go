feecb5baa3f2a1495791c9c3bdfd30c70bfcb9b2
Alec Aivazis <alec@aivazis.com>
2019-02-06 00:56:43 -0800

    dried up gateway tests

--- gateway_test.go
+++ gateway_test.go
@@ -15,7 +15,7 @@ type schemaTableRow struct {
 	query    string
 }
 
-func TestSchema_computeFieldURLs(t *testing.T) {
+func TestGateway(t *testing.T) {
 	schemas := []schemaTableRow{
 		{
 			"url1",
@@ -51,134 +51,137 @@ func TestSchema_computeFieldURLs(t *testing.T) {
 		sources = append(sources, &graphql.RemoteSchema{Schema: schema, URL: source.location})
 	}
 
-	locations := fieldURLs(sources, false)
+	t.Run("Compute Field URLs", func(t *testing.T) {
+		locations := fieldURLs(sources, false)
 
-	allUsersURL, err := locations.URLFor("Query", "allUsers")
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"url1"}, allUsersURL)
+		allUsersURL, err := locations.URLFor("Query", "allUsers")
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"url1"}, allUsersURL)
 
-	lastNameURL, err := locations.URLFor("User", "lastName")
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"url1", "url2"}, lastNameURL)
+		lastNameURL, err := locations.URLFor("User", "lastName")
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"url1", "url2"}, lastNameURL)
 
-	firstNameURL, err := locations.URLFor("User", "firstName")
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"url1"}, firstNameURL)
+		firstNameURL, err := locations.URLFor("User", "firstName")
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"url1"}, firstNameURL)
 
-	// make sure we can look up the url for internal
-	_, ok := locations["__Schema.types"]
-	if !ok {
-		t.Error("Could not find internal type __Schema.types")
-		return
-	}
+		// make sure we can look up the url for internal
+		_, ok := locations["__Schema.types"]
+		if !ok {
+			t.Error("Could not find internal type __Schema.types")
+			return
+		}
 
-	_, ok = locations["Query.__schema"]
-	if !ok {
-		t.Error("Could not find internal field Query.__schema")
-		return
-	}
-}
-
-func TestNew_variadicConfiguration(t *testing.T) {
-
-	schemas := []schemaTableRow{
-		{
-			"url1",
-			`
-				type Query {
-					allUsers: [User!]!
-				}
-
-				type User {
-					firstName: String!
-					lastName: String!
-				}
-			`,
-		},
-		{
-			"url2",
-			`
-				type User {
-					lastName: String!
-				}
-			`,
-		},
-	}
-
-	// the list of remote schemas
-	sources := []*graphql.RemoteSchema{}
-
-	for _, source := range schemas {
-		// turn the combo into a remote schema
-		schema, _ := graphql.LoadSchema(source.query)
-
-		// add the schema to list of sources
-		sources = append(sources, &graphql.RemoteSchema{Schema: schema, URL: source.location})
-	}
-
-	// create a new schema with the sources and some configuration
-	gateway, err := New([]*graphql.RemoteSchema{sources[0]}, func(schema *Gateway) {
-		schema.sources = append(schema.sources, sources[1])
+		_, ok = locations["Query.__schema"]
+		if !ok {
+			t.Error("Could not find internal field Query.__schema")
+			return
+		}
 	})
 
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
+	t.Run("Variadic Configuration", func(t *testing.T) {
+		// create a new schema with the sources and some configuration
+		gateway, err := New([]*graphql.RemoteSchema{sources[0]}, func(schema *Gateway) {
+			schema.sources = append(schema.sources, sources[1])
+		})
 
-	// make sure that the schema has both sources
-	assert.Len(t, gateway.sources, 2)
-}
-
-func TestFieldURLs_ignoreIntrospection(t *testing.T) {
-
-	schemas := []schemaTableRow{
-		{
-			"url1",
-			`
-				type Query {
-					allUsers: [User!]!
-				}
-
-				type User {
-					firstName: String!
-					lastName: String!
-				}
-			`,
-		},
-		{
-			"url2",
-			`
-				type User {
-					lastName: String!
-				}
-			`,
-		},
-	}
-
-	// the list of remote schemas
-	sources := []*graphql.RemoteSchema{}
-
-	for _, source := range schemas {
-		// turn the combo into a remote schema
-		schema, _ := graphql.LoadSchema(source.query)
-
-		// add the schema to list of sources
-		sources = append(sources, &graphql.RemoteSchema{Schema: schema, URL: source.location})
-	}
-
-	locations := fieldURLs(sources, true)
-
-	for key := range locations {
-		if strings.HasPrefix(key, "__") {
-			t.Errorf("Found type starting with __: %s", key)
+		if err != nil {
+			t.Error(err.Error())
+			return
 		}
-	}
 
-	if _, ok := locations["Query.__schema"]; ok {
-		t.Error("Encountered introspection value Query.__schema")
-		return
-	}
+		// make sure that the schema has both sources
+		assert.Len(t, gateway.sources, 2)
+	})
+
+	t.Run("fieldURLs ignore introspection", func(t *testing.T) {
+		locations := fieldURLs(sources, true)
+
+		for key := range locations {
+			if strings.HasPrefix(key, "__") {
+				t.Errorf("Found type starting with __: %s", key)
+			}
+		}
+
+		if _, ok := locations["Query.__schema"]; ok {
+			t.Error("Encountered introspection value Query.__schema")
+			return
+		}
+	})
+
+	t.Run("Configurator WithPlanner", func(t *testing.T) {
+		// the planner we will assign
+		planner := &MockPlanner{}
+
+		gateway, err := New(sources, WithPlanner(planner))
+		if err != nil {
+			t.Error(err.Error())
+			return
+		}
+
+		assert.Equal(t, planner, gateway.planner)
+	})
+
+	t.Run("Response Middleware Error", func(t *testing.T) {
+		// create a new schema with the sources and some configuration
+		gateway, err := New([]*graphql.RemoteSchema{sources[0]},
+			WithExecutor(ExecutorFunc(func(ctx *ExecutionContext) (map[string]interface{}, error) {
+				return map[string]interface{}{"goodbye": "moon"}, nil
+			})),
+			WithMiddleware(
+				ResponseMiddleware(func(ctx *ExecutionContext, response map[string]interface{}) error {
+					return errors.New("this string")
+				}),
+			))
+		if err != nil {
+			t.Error(err.Error())
+			return
+		}
+
+		// build a query plan that the executor will follow
+		_, err = gateway.Execute(context.Background(), `{ allUsers { firstName } }`, map[string]interface{}{})
+		if err == nil {
+			t.Errorf("Did not encounter error executing plan.")
+		}
+	})
+
+	t.Run("Response Middleware Success", func(t *testing.T) {
+
+		// create a new schema with the sources and some configuration
+		gateway, err := New([]*graphql.RemoteSchema{sources[0]},
+			WithExecutor(ExecutorFunc(func(ctx *ExecutionContext) (map[string]interface{}, error) {
+				return map[string]interface{}{"goodbye": "moon"}, nil
+			})),
+			WithMiddleware(
+				ResponseMiddleware(func(ctx *ExecutionContext, response map[string]interface{}) error {
+					// clear the previous value
+					for k := range response {
+						delete(response, k)
+					}
+
+					// set something we can test against
+					response["hello"] = "world"
+
+					// no errors
+					return nil
+				}),
+			))
+		if err != nil {
+			t.Error(err.Error())
+			return
+		}
+
+		// build a query plan that the executor will follow
+		response, err := gateway.Execute(context.Background(), `{ allUsers { firstName } }`, map[string]interface{}{})
+
+		if err != nil {
+			t.Errorf("Encountered error executing plan: %s", err.Error())
+			return
+		}
+		// make sure our middleware changed the response
+		assert.Equal(t, map[string]interface{}{"hello": "world"}, response)
+	})
 }
 
 func TestFieldURLs_concat(t *testing.T) {
@@ -218,160 +221,4 @@ func TestFieldURLs_concat(t *testing.T) {
 		return
 	}
 	assert.Equal(t, []string{"url2"}, urlLocations3)
-}
-
-func TestSchemaConfigurator_withPlanner(t *testing.T) {
-	schema, _ := graphql.LoadSchema(
-		`
-			type Query {
-				allUsers: [String!]!
-			}
-		`,
-	)
-
-	remoteSchema := &graphql.RemoteSchema{
-		Schema: schema,
-		URL:    "hello",
-	}
-
-	// the planner we will assign
-	planner := &MockPlanner{}
-
-	gateway, err := New([]*graphql.RemoteSchema{remoteSchema}, WithPlanner(planner))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-
-	assert.Equal(t, planner, gateway.planner)
-}
-
-func TestGateway_responseMiddlewareError(t *testing.T) {
-	schemas := []schemaTableRow{
-		{
-			"url1",
-			`
-				type Query {
-					allUsers: [User!]!
-				}
-
-				type User {
-					firstName: String!
-					lastName: String!
-				}
-			`,
-		},
-		{
-			"url2",
-			`
-				type User {
-					lastName: String!
-				}
-			`,
-		},
-	}
-
-	// the list of remote schemas
-	sources := []*graphql.RemoteSchema{}
-
-	for _, source := range schemas {
-		// turn the combo into a remote schema
-		schema, _ := graphql.LoadSchema(source.query)
-
-		// add the schema to list of sources
-		sources = append(sources, &graphql.RemoteSchema{Schema: schema, URL: source.location})
-	}
-
-	// create a new schema with the sources and some configuration
-	gateway, err := New([]*graphql.RemoteSchema{sources[0]},
-		WithExecutor(ExecutorFunc(func(ctx *ExecutionContext) (map[string]interface{}, error) {
-			return map[string]interface{}{"goodbye": "moon"}, nil
-		})),
-		WithMiddleware(
-			ResponseMiddleware(func(ctx *ExecutionContext, response map[string]interface{}) error {
-				return errors.New("this string")
-			}),
-		))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-
-	// build a query plan that the executor will follow
-	_, err = gateway.Execute(context.Background(), `{ allUsers { firstName } }`, map[string]interface{}{})
-	if err == nil {
-		t.Errorf("Did not encounter error executing plan.")
-	}
-}
-
-func TestGateway_responseMiddlewareSuccess(t *testing.T) {
-
-	schemas := []schemaTableRow{
-		{
-			"url1",
-			`
-				type Query {
-					allUsers: [User!]!
-				}
-
-				type User {
-					firstName: String!
-					lastName: String!
-				}
-			`,
-		},
-		{
-			"url2",
-			`
-				type User {
-					lastName: String!
-				}
-			`,
-		},
-	}
-
-	// the list of remote schemas
-	sources := []*graphql.RemoteSchema{}
-
-	for _, source := range schemas {
-		// turn the combo into a remote schema
-		schema, _ := graphql.LoadSchema(source.query)
-
-		// add the schema to list of sources
-		sources = append(sources, &graphql.RemoteSchema{Schema: schema, URL: source.location})
-	}
-
-	// create a new schema with the sources and some configuration
-	gateway, err := New([]*graphql.RemoteSchema{sources[0]},
-		WithExecutor(ExecutorFunc(func(ctx *ExecutionContext) (map[string]interface{}, error) {
-			return map[string]interface{}{"goodbye": "moon"}, nil
-		})),
-		WithMiddleware(
-			ResponseMiddleware(func(ctx *ExecutionContext, response map[string]interface{}) error {
-				// clear the previous value
-				for k := range response {
-					delete(response, k)
-				}
-
-				// set something we can test against
-				response["hello"] = "world"
-
-				// no errors
-				return nil
-			}),
-		))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-
-	// build a query plan that the executor will follow
-	response, err := gateway.Execute(context.Background(), `{ allUsers { firstName } }`, map[string]interface{}{})
-
-	if err != nil {
-		t.Errorf("Encountered error executing plan: %s", err.Error())
-		return
-	}
-	// make sure our middleware changed the response
-	assert.Equal(t, map[string]interface{}{"hello": "world"}, response)
 }
