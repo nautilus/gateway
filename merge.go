dc5f45c468791fbd697a83083ae1c0810d8b2e07
Andreas Raster <lazor@affenbande.org>
2020-03-04 16:32:00 +0100

    remove the check if field descriptions match when merging schemas

--- merge.go
+++ merge.go
@@ -388,11 +388,6 @@ func mergeFieldListEqual(list1, list2 ast.FieldList) error {
 }
 
 func mergeFieldsEqual(field1, field2 *ast.FieldDefinition) error {
-	// if the 2 descriptions don't match
-	if field1.Description != "The ID of the object." && field2.Description != "The ID of the object." && field1.Description != field2.Description {
-		return fmt.Errorf("conflict in field descriptions. Found \"%v\" and \"%v\"", field1.Description, field2.Description)
-	}
-
 	// fields
 	if err := mergeTypesEqual(field1.Type, field2.Type); err != nil {
 		return fmt.Errorf("fields are not equal: %v", err.Error())
