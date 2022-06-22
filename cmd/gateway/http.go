c90f1babe3a821420a509c3a73f1ddd0121d8ed2
John Starich <johnstarich@johnstarich.com>
2022-06-22 00:19:30 -0500

    Update cmd/gateway to serve UI on / and API on /graphql

--- cmd/gateway/http.go
+++ cmd/gateway/http.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/nautilus/gateway"
 	"github.com/nautilus/graphql"
@@ -25,19 +26,25 @@ func ListenAndServe(services []string) {
 	}
 
 	// add the graphql endpoints to the router
-	http.HandleFunc("/graphql", setCORSHeaders(gw.PlaygroundHandler))
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && strings.Contains(r.Header.Get("Accept"), "text/html") { // rudimentary check to see if this is accessed from a browser UI
+			// if calling from a UI, redirect to the UI handler
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+		gw.GraphQLHandler(w, r)
+	})
 
-	// start the server
-	fmt.Printf("🚀 Gateway is ready at http://localhost:%s/graphql\n", Port)
-	err = http.ListenAndServe(fmt.Sprintf(":%s", Port), nil)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-}
+	playgroundHandler := gw.StaticPlaygroundHandler(gateway.PlaygroundConfig{
+		Endpoint: "/graphql",
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			// ensure our catch-all handler pattern "/" only runs on "/"
+			http.NotFound(w, r)
+			return
+		}
 
-func setCORSHeaders(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
 		// set the necessary CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -45,11 +52,18 @@ func setCORSHeaders(fn http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		// if we are handling a pre-flight request
-		if req.Method == http.MethodOptions {
+		if r.Method == http.MethodOptions {
 			return
 		}
 
-		// invoke the handler
-		fn(w, req)
+		playgroundHandler.ServeHTTP(w, r)
+	})
+
+	// start the server
+	fmt.Printf("🚀 Gateway is ready at http://localhost:%s/graphql\n", Port)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", Port), nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 }
