bd8a71a3a7c1edcaafc03c070cca5f62bc406cb1
John Starich <johnstarich@johnstarich.com>
2025-10-16 17:34:37 -0500

    Fix CVE-2025-11065
    This reverts commit fa8e1a070ec903945d66379ec692f5be36fba92f.

--- internal.go
+++ internal.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/99designs/gqlgen/graphql/introspection"
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
 	"github.com/vektah/gqlparser/v2/gqlerror"
