ab4516fe80a2d8a36c0780e0a81bdd37f48ab492
John Starich <johnstarich@johnstarich.com>
2026-04-09 14:38:25 -0500

    Revert plan changes for now

--- plan_test.go
+++ plan_test.go
@@ -2,70 +2,12 @@ package gateway
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/nautilus/graphql"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"github.com/vektah/gqlparser/v2/ast"
 )
-
-func logPlans(tb testing.TB, plans []*QueryPlan) {
-	tb.Helper()
-	tb.Log(dumpPlans(plans))
-}
-
-const dumpIndent = "  "
-
-func addIndent(s string) string {
-	var builder strings.Builder
-	for line := range strings.SplitAfterSeq(s, "\n") {
-		builder.WriteString(dumpIndent)
-		builder.WriteString(line)
-	}
-	return builder.String()
-}
-
-func dumpPlans(plans []*QueryPlan) string {
-	var builder strings.Builder
-	for index, plan := range plans {
-		builder.WriteString(fmt.Sprintf("plan %d:\n", index))
-		builder.WriteString(addIndent(dumpPlan(*plan)))
-	}
-	return builder.String()
-}
-
-func dumpPlan(plan QueryPlan) string {
-	var builder strings.Builder
-	builder.WriteString("root step:\n")
-	builder.WriteString(dumpPlanStep(*plan.RootStep))
-	builder.WriteString(addIndent(dumpPlanSteps(plan.RootStep.Then)))
-	return builder.String()
-}
-
-func dumpPlanSteps(steps []*QueryPlanStep) string {
-	var builder strings.Builder
-	for index, step := range steps {
-		kind := "unknown"
-		switch queryer := step.Queryer.(type) {
-		case *graphql.SingleRequestQueryer:
-			kind = queryer.URL()
-		case *Gateway:
-			kind = "internal"
-		}
-		builder.WriteString(fmt.Sprintf("then step %d (%s):\n", index, kind))
-		builder.WriteString(addIndent(dumpPlanStep(*step)))
-		builder.WriteString(addIndent(dumpPlanSteps(step.Then)))
-	}
-	return builder.String()
-}
-
-func dumpPlanStep(step QueryPlanStep) string {
-	return fmt.Sprintf(`(parent: %s)
-%v
-`, step.ParentType, strings.ReplaceAll(step.QueryString, "\t", dumpIndent))
-}
 
 func TestPlanQuery_singleRootField(t *testing.T) {
 	t.Parallel()
@@ -693,6 +635,7 @@ func TestPlanQuery_singleRootObject(t *testing.T) {
 		t.Error("Did not get an  inner firstName out of the allUsers selection")
 	}
 	assert.Equal(t, "firstName", firstNameInnerField.Name)
+
 }
 
 func TestPlanQuery_subGraphs(t *testing.T) {
@@ -1117,12 +1060,21 @@ func TestPlanQuery_groupSiblings(t *testing.T) {
 
 func TestPlanQuery_nodeField(t *testing.T) {
 	t.Parallel()
+	// the query to test
+	// query {
+	//     node(id: $id) {
+	//     		... on User {
+	//				firstName
+	//				lastName
+	//    		}
+	//     }
+	// }
+
 	// the location map for fields for this query
 	locations := FieldURLMap{}
-	locations.RegisterURL("User", "id", "url1", "url2")
 	locations.RegisterURL("User", "firstName", "url1")
 	locations.RegisterURL("User", "lastName", "url2")
-	locations.RegisterURL(typeNameQuery, "node", "url1", "url2")
+	locations.RegisterURL(typeNameQuery, "node", "url1", "url2", internalSchemaLocation)
 
 	// load the query we're going to query
 	schema, err := graphql.LoadSchema(`
@@ -1146,12 +1098,10 @@ func TestPlanQuery_nodeField(t *testing.T) {
 
 	// plan the query
 	plans, err := (&MinQueriesPlanner{}).Plan(&PlanningContext{
-		// the query to test
 		Query: `
 			query($id: ID!) {
 				node(id: $id) {
 					... on User {
-						id
 						firstName
 						lastName
 					}
@@ -1166,18 +1116,23 @@ func TestPlanQuery_nodeField(t *testing.T) {
 		t.Error(err.Error())
 		return
 	}
-	logPlans(t, plans)
 
 	// we should return only one plan
-	require.Len(t, plans, 1)
+	if !assert.Len(t, plans, 1) {
+		return
+	}
 
 	// this plan should have 1 step that should hit the internal API
-	require.Len(t, plans[0].RootStep.Then, 1, "incorrect number of steps in plan")
-	require.IsType(t, &Gateway{}, plans[0].RootStep.Then[0].Queryer, "first step does not go to the internal API")
+	if !assert.Len(t, plans[0].RootStep.Then, 1, "incorrect number of steps in plan") ||
+		!assert.IsType(t, &Gateway{}, plans[0].RootStep.Then[0].Queryer, "first step does not go to the internal API") {
+		return
+	}
 	internalStep := plans[0].RootStep.Then[0]
 
 	// the step should have 2 after it
-	require.Len(t, internalStep.Then, 2)
+	if !assert.Len(t, internalStep.Then, 2) {
+		return
+	}
 
 	// grab the 2 steps
 	var url1Step *QueryPlanStep
@@ -1189,8 +1144,9 @@ func TestPlanQuery_nodeField(t *testing.T) {
 			url2Step = step
 		}
 	}
-	require.NotNil(t, url1Step)
-	require.NotNil(t, url2Step)
+	if !assert.NotNil(t, url1Step) || !assert.NotNil(t, url2Step) {
+		return
+	}
 
 	t.Run("Url1 Step", func(t *testing.T) {
 		t.Parallel()
@@ -1237,137 +1193,6 @@ func TestPlanQuery_nodeField(t *testing.T) {
 		}
 		assert.Equal(t, "lastName", graphql.SelectedFields(inlineFragment.SelectionSet)[0].Name)
 	})
-}
-
-func TestPlanQuery_nodeFieldOnlyRequestedFromOwningService(t *testing.T) {
-	t.Parallel()
-	locations := FieldURLMap{}
-	locations.RegisterURL(typeNameQuery, "node", "url1", "url2") // url1 can resolve Foos, but not Bars. MUST NOT receive a request for Bars.
-	locations.RegisterURL("Foo", "id", "url1")                   // Foos resolvable by url1
-	locations.RegisterURL("Bar", "id", "url2")                   // Bars resolvable by url2
-
-	schema, err := graphql.LoadSchema(`
-		interface Node {
-			id: ID!
-		}
-		type Query {
-			node(id: ID!): Node
-		}
-
-		type Foo implements Node {  # Only registered to url1
-			id: ID!
-		}
-		type Bar implements Node {  # Only registered to url2
-			id: ID!
-		}
-	`)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	plans, err := (&MinQueriesPlanner{}).Plan(&PlanningContext{
-		Query: `
-			query($id: ID!) {
-				node(id: $id) {
-					... on Bar {
-						id
-					}
-				}
-			}
-		`,
-		Schema:    schema,
-		Locations: locations,
-		Gateway:   &Gateway{logger: &DefaultLogger{}},
-	})
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	logPlans(t, plans)
-	require.Len(t, plans, 1)
-
-	// this plan should have 1 step that only hits url2
-	require.Len(t, plans[0].RootStep.Then, 1, "incorrect number of steps in plan")
-	step := plans[0].RootStep.Then[0]
-	require.IsType(t, &graphql.SingleRequestQueryer{}, step.Queryer, "first step must go to url2")
-	queryer := step.Queryer.(*graphql.SingleRequestQueryer)
-	assert.Equal(t, "url2", queryer.URL())
-	assert.Empty(t, step.Then)
-}
-
-func TestPlanQuery_nodeFieldFromOnlyOneSchema(t *testing.T) {
-	t.Parallel()
-	locations := FieldURLMap{}
-	locations.RegisterURL("User", "id", "url1", "url2") // Should not request url1 when same id field can be resolved by url2.
-	locations.RegisterURL(typeNameQuery, "node", "url1", "url2")
-	locations.RegisterURL("User", "firstName", "url2")
-
-	schema, err := graphql.LoadSchema(`
-		interface Node {
-			id: ID!
-		}
-
-		type User implements Node {
-			id: ID!             # Both url1 and url2
-			firstName: String!  # Only url2.
-		}
-
-		type Query {
-			node(id: ID!): Node
-		}
-	`)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	plans, err := (&MinQueriesPlanner{}).Plan(&PlanningContext{
-		Query: `
-			query($id: ID!) {
-				node(id: $id) {
-					... on User {
-						firstName
-					}
-				}
-			}
-		`,
-		Schema:    schema,
-		Locations: locations,
-		Gateway:   &Gateway{logger: &DefaultLogger{}},
-	})
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	logPlans(t, plans)
-	require.Len(t, plans, 1)
-
-	// this plan should have 1 step that only hits url2
-	require.Len(t, plans[0].RootStep.Then, 1, "incorrect number of steps in plan")
-	step := plans[0].RootStep.Then[0]
-	require.IsType(t, &graphql.SingleRequestQueryer{}, step.Queryer, "first step must go to url2")
-	queryer := step.Queryer.(*graphql.SingleRequestQueryer)
-	assert.Equal(t, "url2", queryer.URL())
-	assert.Empty(t, step.Then)
-
-	// the url2 step should have Node as the parent type
-	assert.Equal(t, "Node", step.ParentType)
-	// there should be one selection set
-	if !assert.Len(t, step.SelectionSet, 1) {
-		return
-	}
-
-	// it should be an inline fragment on User
-	inlineFragment, ok := step.SelectionSet[0].(*ast.InlineFragment)
-	if !assert.True(t, ok) {
-		return
-	}
-	assert.Equal(t, "User", inlineFragment.TypeCondition)
-
-	// with one selection set: firstName
-	if !assert.Len(t, inlineFragment.SelectionSet, 1) {
-		return
-	}
-	assert.Equal(t, "firstName", graphql.SelectedFields(inlineFragment.SelectionSet)[0].Name)
 }
 
 func TestPlanQuery_stepVariables(t *testing.T) {
@@ -1640,6 +1465,7 @@ func TestPlanQuery_singleFragmentMultipleLocations(t *testing.T) {
 	}
 
 	assert.Equal(t, "lastName", userInfoSelection.Name)
+
 }
 
 func TestPlannerBuildQuery_query(t *testing.T) {
@@ -1826,6 +1652,7 @@ func TestPlanQuery_forcedPriorityResolution(t *testing.T) {
 			Locations: locations,
 			Gateway:   &Gateway{logger: &DefaultLogger{}},
 		})
+
 		if err != nil {
 			return nil, fmt.Errorf("encountered error when planning query: %w", err)
 		}
